20040d19e45a09bfb3e6a68adb8c4902744d01a5
kkjin85 <kkjin85@gmail.com>
2024-10-22 01:32:06 -0500

    update index.html

--- ArtBash_Sketch_2024_10_22_05_54_53/sketch.js
+++ ArtBash_Sketch_2024_10_22_05_54_53/sketch.js
@@ -9,7 +9,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(795, 795);
+  createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
   cols = width / size;
   rows = height / size;
